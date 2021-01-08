7d7e9b8d10c21df38da68a6db3f1ac6a952eaa71
deshan perera <deshkperera@gmail.com>
2021-01-08 09:49:43 +0530

    ***

---
+++ Assets/Scripts/EndPointTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EndPointTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    private void OnTriggerEnter(Collider other)
+    {
+        gameManager.CompleteLevel();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over!");
+            Restart();
+            
+
+        }
+    }
+
+     void Restart()
+    {
+        StartCoroutine(Restart1());
+    }
+
+    IEnumerator Restart1()
+    {
+        yield return new WaitForSeconds(restartDelay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
